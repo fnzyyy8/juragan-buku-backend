d315c2a209aec0d024bf682e3d58b00353d93d67
fnzyyy8 <farhanshidayat@gmail.com>
2024-01-09 15:51:48 +0700

    Menggunakan Joi sebagai validation

--- tests/addCostumers.test.js
+++ tests/addCostumers.test.js
@@ -1,20 +1,39 @@
 import { prismaClient } from "../src/prisma-client";
+import Joi from "joi";
 
 describe("Add Costumers", () => {
   test("should create costumers", async () => {
-    const costumers = await prismaClient.pembeli.createMany({
-      data: [
-        {
-          name: "Siska Anita",
-          username: "Siska888",
-          password: "Siska123",
-        },
-        {
-          name: "Firli Rinjani",
-          username: "Firli888",
-          password: "Firli123",
-        },
-      ],
+    const costumers = [
+      {
+        name: "Den",
+        username: "Deni888",
+        password: "",
+      },
+      {},
+    ];
+
+    const schemaObject = Joi.object({
+      name: Joi.string().min(10).required(),
+      username: Joi.string().max(10).required(),
+      password: Joi.string().min(8).required(),
     });
+
+    for (const customer of costumers) {
+      const result = schemaObject.validate(customer, { abortEarly: false });
+
+      if (result.error) {
+        throw new Error(
+          result.error.details
+            .map((value) => `${value.path} : ${value.message}`)
+            .join("\n")
+        );
+      } else {
+        const uploded = await Promise.all(
+          costumers.map((costumer) =>
+            prismaClient.pembeli.create({ data: costumer })
+          )
+        );
+      }
+    }
   });
 });
